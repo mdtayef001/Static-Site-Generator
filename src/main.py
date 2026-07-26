4f451420ee719086d77d54ab6be8949117344918
tayef <tayefhossain200@gmail.com>
2026-07-26 22:27:35 +0600

    copy static to public files

--- src/main.py
+++ src/main.py
@@ -1,13 +1,19 @@
-from textnode import TextType, TextNode
-from htmlnode import LeafNode
+import os
+import shutil
+
+from copystatic import copy_files_recursive
+
+dir_path_static = "./static"
+dir_path_public = "./public"
 
 
 def main() -> None:
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
 
-    test = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    leaf_node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
-
-    print(leaf_node.to_html())
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
 
 
 main()
